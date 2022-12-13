e43c9bfb8edbe6037757a5b5fe45df0cd3c74d09
lkangr <dangquocthang721@gmail.com>
2022-12-13 11:06:56 +0700

    build

--- Assets/_Completed-Assets/Scripts/Managers/TankManagerAI.cs
+++ Assets/_Completed-Assets/Scripts/Managers/TankManagerAI.cs
@@ -134,50 +134,6 @@ namespace Complete
                 }
 
                 if (shotDelay > 0) shotDelay -= Time.deltaTime;
-
-                return;
-                //m_Movement.CallUpdate();
-                // Store the value of both input axes.
-                m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
-                m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
-
-
-                //m_Shooting.CallUpdate();
-                // The slider should have a default value of the minimum launch force.
-                m_AimSlider.value = m_MinLaunchForce;
-
-                // If the max force has been exceeded and the shell hasn't yet been launched...
-                if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
-                {
-                    // ... use the max force and launch the shell.
-                    m_CurrentLaunchForce = m_MaxLaunchForce;
-                    Fire();
-                }
-                // Otherwise, if the fire button has just started being pressed...
-                else if (Input.GetButtonDown(m_FireButton))
-                {
-                    // ... reset the fired flag and reset the launch force.
-                    m_Fired = false;
-                    m_CurrentLaunchForce = m_MinLaunchForce;
-
-                    // Change the clip to the charging clip and start it playing.
-                    m_ShootingAudio.clip = m_ChargingClip;
-                    m_ShootingAudio.Play();
-                }
-                // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
-                else if (Input.GetButton(m_FireButton) && !m_Fired)
-                {
-                    // Increment the launch force and update the slider.
-                    m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
-
-                    m_AimSlider.value = m_CurrentLaunchForce;
-                }
-                // Otherwise, if the fire button is released and the shell hasn't been launched yet...
-                else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
-                {
-                    // ... launch the shell.
-                    Fire();
-                }
             }
 
             EngineAudio();
